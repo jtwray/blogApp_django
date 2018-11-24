c4abd7592550bd0d97dafd083d1106982ec1703d
Tucker Wray <tuckerwray84@gmail.com>
2018-11-23 23:22:52 -0500

    Bootstrap/DRY principle in Django with extensions[ implemented block coding ==>base.html ]

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,6 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    
 
     def publish(self):
         self.published_date = timezone.now()
