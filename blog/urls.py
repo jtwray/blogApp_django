7ffd02a05820f3c283735419c50222091fdf3d1e
Tucker Wray <tuckerwray84@gmail.com>
2018-11-25 15:06:00 -0500

    Add feature to create new blog posts from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.postsList, name='postsList'),
     # 127.0.0.1:8000/post/2 --> local
     # mydjangosite.com/post/2 --> online
-    path('post/<int:pk>/', views.postsDetail, name='postsDetail')
+    path('post/<int:pk>/', views.postsDetail, name='postsDetail'),
+    # 127.0.0.1:8000/post/New --> local
+    # mydjangosite.com/post/new--> online
+    path('post/new/', views.postNew, name='postNew'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 # Create your views here.
 def postsList(request):
@@ -11,3 +12,17 @@ def postsDetail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_for_frontend = {'post': post}
     return render(request, 'blog/postsdetail.html', stuff_for_frontend)
+
+def postNew(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('postsDetail', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+    return render(request, 'blog/postEdit.html', stuff_for_frontend)
