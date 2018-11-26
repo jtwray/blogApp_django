82249673ef6b5dac5719760b9d7d6ef1cbd2f8d0
Tucker Wray <tuckerwray84@gmail.com>
2018-11-25 23:11:51 -0500

    Added funtionality to Edit from front end.

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     # 127.0.0.1:8000/post/2 --> local
     # mydjangosite.com/post/2 --> online
     path('post/<int:pk>/', views.postsDetail, name='postsDetail'),
-    # 127.0.0.1:8000/post/New --> local
+    # 127.0.0.1:8000/post/new --> local
     # mydjangosite.com/post/new--> online
     path('post/new/', views.postNew, name='postNew'),
+    # 127.0.0.1:8000/post/1/edit --> local
+    # mydjangosite.com/post/1/edit--> online
+    path('post/<int:pk>/edit/', views.postEdit, name='postEdit'),
 ]
